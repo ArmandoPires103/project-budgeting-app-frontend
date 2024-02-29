099b014e426f3171cb0b5c4a617de1ac06736a52
armandopires <armandopires@pursuit.org>
2024-02-29 12:31:15 -0500

    Added transactions css

--- src/Transactions.jsx
+++ src/Transactions.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Transactions.css'
 
 const Transactions = ({ transactions, setTransactions }) => {
   if (transactions.length === 0) return null;
@@ -13,20 +14,20 @@ const Transactions = ({ transactions, setTransactions }) => {
       .then((data) => setTransactions(data.transactions))
   }
   return (
-    <div>
-      <h1>Transactions</h1>
+    <div className="transactions-container"> {/* Apply transactions container style */}
+      <h1 className='transaction-header'>Transactions</h1>
       {transactions.map(({ id, item_name, amount, date, from, category }) => (
-        <div key={id}>
-          <h3>Item Name: {item_name}</h3>
-          <p>Date: {date}</p>
-          <p>Amount: {amount}</p>
+        <div className="transaction-item" key={id}> {/* Apply transaction item style */}
+          <h3 className="transaction-title">Item Name: {item_name}</h3> {/* Apply transaction title style */}
+          <p className="transaction-details">Date: {date}</p> {/* Apply transaction details style */}
+          <p className="transaction-details">Amount: {amount}</p> {/* Apply transaction details style */}
           <Link to={`/${id}`}>
-            <button>Details</button>
+            <button className="transaction-button">Details</button> {/* Apply transaction button style */}
           </Link>
           <Link to={`/edit/${id}`}>
-            <button>Edit</button>
+            <button className="transaction-button">Edit</button> {/* Apply transaction button style */}
           </Link>
-          <button onClick={() => handleDelete(id)}>Delete</button>
+          <button className="transaction-button delete-button" onClick={() => handleDelete(id)}>Delete</button> {/* Apply delete button style */}
         </div>
       ))}
     </div>
